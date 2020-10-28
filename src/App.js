cdd232906d9c2f17e9ce971ac35d9935dd76d8af
bartxs <b.soltysik90@gmail.com>
2020-10-28 15:13:57 +0100

    SPA-Components done

--- src/App.js
+++ src/App.js
@@ -3,43 +3,35 @@ import React from 'react';
 import Avatar from './Avatar';
 
 function App() {
+
   const data =[
-    {size:"s",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"l",type:"sqaure"},
-    {size:"m",type:"cicle"},
-    {size:"xl",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"l",type:"sqaure"},
-    {size:"m",type:"cicle"},
-    {size:"xl",type:"rounded"}
+    {size:"S with 30x30",type:"square"},
+    {size:"S with 30x30",type:"rounded"},
+    {size:"S with 30x30",type:"circle"},
+    {size:"M with 60x60",type:"square"},
+    {size:"M with 60x60",type:"rounded"},
+    {size:"M with 60x60",type:"circle"},
+    {size:"L with 120x120",type:"square"},
+    {size:"L with 120x120",type:"rounded"},
+    {size:"L with 120x120",type:"circle"},
+    {size:"XL with 200x200",type:"square"},
+    {size:"XL with 200x200",type:"rounded"},
+    {size:"XL with 200x200",type:"circle"}
   ]
+
   return (
     <div className="App">
+
       <h1>My React App</h1>
+
       <div style={{"display":"flex","flexWrap":"wrap"}}> 
-      {data.map(item=>{
-        return (
-          <Avatar size={item.size} type={item.type}/>
-        )
-      })}
-
-
-
-
-
-
-
-      {/* <Avatar size="s" type="circle"/>
-      <Avatar size="l" type="rounded"/>
-      <Avatar />
-      <Avatar size="l" type="rounded"/>
-      <Avatar />
-      <Avatar size="l" type="rounded"/>
-      <Avatar /> */}
-
+        {data.map(item=>{
+          return (
+            <Avatar size={item.size} type={item.type}/>
+          )
+        })} 
       </div>
+
     </div>
   );
 }
