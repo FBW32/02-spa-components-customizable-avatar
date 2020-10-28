9b4372139a337e90387a1c5c38b2522df958e29d
ben <beatuscanis@hotmail.com>
2020-10-28 14:54:04 +0100

    done

--- src/App.js
+++ src/App.js
@@ -3,42 +3,34 @@ import React from 'react';
 import Avatar from './Avatar';
 
 function App() {
-  const data =[
-    {size:"s",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"l",type:"sqaure"},
-    {size:"m",type:"cicle"},
-    {size:"xl",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"s",type:"rounded"},
-    {size:"l",type:"sqaure"},
-    {size:"m",type:"cicle"},
-    {size:"xl",type:"rounded"}
+  const data = [
+    // ROUNDED
+    { size: "s", type: "rounded" },
+    { size: "m", type: "rounded" },
+    { size: "l", type: "rounded" },
+    { size: "xl", type: "rounded" },
+
+    // CIRCLE
+    { size: "s", type: "circle" },
+    { size: "m", type: "circle" },
+    { size: "l", type: "circle" },
+    { size: "xl", type: "circle" },
+
+    // SQUARE
+    { size: "s", type: "square" },
+    { size: "m", type: "square" },
+    { size: "l", type: "square" },
+    { size: "xl", type: "square" }
   ]
   return (
     <div className="App">
       <h1>My React App</h1>
-      <div style={{"display":"flex","flexWrap":"wrap"}}> 
-      {data.map(item=>{
-        return (
-          <Avatar size={item.size} type={item.type}/>
-        )
-      })}
-
-
-
-
-
-
-
-      {/* <Avatar size="s" type="circle"/>
-      <Avatar size="l" type="rounded"/>
-      <Avatar />
-      <Avatar size="l" type="rounded"/>
-      <Avatar />
-      <Avatar size="l" type="rounded"/>
-      <Avatar /> */}
-
+      <div style={{ "display": "flex", "flexWrap": "wrap" }} >
+        {data.map(item => {
+          return (
+            <Avatar size={item.size} type={item.type} />
+          )
+        })}
       </div>
     </div>
   );
